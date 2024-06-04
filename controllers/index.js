7b912b07d020ea970b6989aaee2b9ac3f624b455
Aurlien-V <153271619+Aurlien-V@users.noreply.github.com>
2024-06-04 11:26:58 +0200

    v1

---
+++ controllers/index.js
@@ -0,0 +1,37 @@
+const Student = require('../models/students');
+
+const data = {
+    students: ["Théo", "Léo", "Matthéo", "Adrien", "Maxence"]
+}
+
+const getRandomStudent = () => {
+    const random = Math.floor(Math.random() * data.students.length);
+    return data.students[random];
+}
+    
+
+
+const controlStudent = {
+    random : (req, res) => {
+        const randomStudent = getRandomStudent();
+        res.status(200).json({ stake: randomStudent });
+    },
+
+    create: async (req, res) => {
+        console.log(req.body);
+        const data = await Student.create(req.body);
+           console.log(data);
+           res.status(201).json({ message : "Etudiant créé" , data: req.body});
+       },
+
+    findAll : async (req, res) => {
+        const data = await Student.findAll();
+        res.status(200).json({students : data});
+    }
+
+
+
+
+}
+
+module.exports = controlStudent;
